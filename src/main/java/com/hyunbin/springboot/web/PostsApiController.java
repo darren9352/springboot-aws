97076209c668b5c5ae1359c0eb8868f0bdb8da0d
darren9352 <darren9352@gmail.com>
2020-03-01 03:03:14 +0900

    add post update test

---
+++ src/main/java/com/hyunbin/springboot/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.hyunbin.springboot.web;
+
+import com.hyunbin.springboot.service.posts.PostsService;
+import com.hyunbin.springboot.web.dto.PostsResponseDto;
+import com.hyunbin.springboot.web.dto.PostsSaveRequestDto;
+import com.hyunbin.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return  postsService.findById(id);
+    }
+}
